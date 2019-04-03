6f459834d247b13befb452d358c007213d6f7f53
Cluttered04 <sbpwem@gmail.com>
2019-04-03 09:34:48 -0400

    set up navbar for authentication

--- src/components/Nav/NavBar.js
+++ src/components/Nav/NavBar.js
@@ -1,11 +1,26 @@
 import React, {Component} from "react"
-import {Link} from "react-router-dom"
+import { Link, withRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css"
+import auth0Client from "../authentication/Auth";
 
 class NavBar extends Component {
+
+    signOut = () => {
+        auth0Client.signOut();
+        sessionStorage.clear()
+        this.props.history.replace("/");
+      };
+
+
     render(){
         return (
-            <nav className="navbar navbar-light light-blue flex-md-nowrap p-0 shadow">
+        <nav className="navbar navbar-light light-blue flex-md-nowrap p-0 shadow">
+        {!auth0Client.isAuthenticated() ? (
+            <button className="btn btn-success" onClick={auth0Client.signIn}>
+             Sign In
+            </button>
+        ) : (
+        <React.Fragment>
             <ul className="nav nav-pills nav-fill decNav">
                 <li className="nav-item">
                     <Link className="nav-link" to="/">Home</Link>
@@ -22,14 +37,27 @@ class NavBar extends Component {
                         "nav-item">
                         <Link className="nav-link" to="/login" onClick={this.props.handleLogout}>Log Out</Link>
                     </li>
-
-            </ul>
+                    </ul>
+                    <div>
+                    <label className="mr-2 text-blue">
+                        {auth0Client.getProfile().name}
+                    </label>
+                    <button
+                        className="btn btn-danger"
+                        onClick={() => {
+                            this.signOut();
+                     }}
+                    >
+                    Sign Out
+                    </button>
+            </div>
+        </React.Fragment>
+        )}
         </nav>
-            )
-        }
+      );
     }
+   }
 
 
 
-
-export default NavBar
+    export default withRouter(NavBar);
