a3b18c41cc22d830290981fbe981f1e8d32baa0b
Cluttered04 <sbpwem@gmail.com>
2019-04-03 09:25:15 -0400

    set up callback, gitignore, and import/render statement in fitpup.js

--- src/components/FitPup.js
+++ src/components/FitPup.js
@@ -2,8 +2,20 @@ import React, {Component} from "react";
 import ApplicationViews from "./ApplicationViews"
 import "./FitPup.css";
 import NavBar from "./Nav/NavBar"
+import auth0Client from "./authentication/Auth"
 
 class FitPup extends Component {
+
+    async componentDidMount() {
+        if (this.props.location.pathname === '/callback') return;
+        try {
+          await auth0Client.silentAuth();
+          this.forceUpdate();
+        } catch (err) {
+          if (err.error !== 'login_required') console.log(err.error);
+        }
+      }
+
     render() {
         return (
             <React.Fragment>
